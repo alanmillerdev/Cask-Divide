cbbaa7859749655283b2b52f035ffa0e560767f8
EuanMaxwell2001 <euanmax@gmail.com>
2022-03-07 15:11:24 +0000

    Updated styling on home page (still to fix cask position)

--- index.php
+++ index.php
@@ -17,42 +17,48 @@ include 'Components/RequiredComponents/bootstrapCSS.inc.php';
   <title>Scotch Whiskey Casks</title>
 </head>
 
-<!--NAVBAR INCLUDE-->
+<div class="content">
+
+    <!--NAVBAR INCLUDE-->
     <?php
     $page='home';
     include('./includes/navbar_login.php');
     ?>
 
-  <!-- HOME SECTION -->
-  <section id="home-section">
-    <div class="home-inner container">
-      <div class="row">
-        <div class="col-lg-8 d-none d-lg-block">
-          <h1 class="display-4" id="main-title">INVEST IN SCOTCH WHISKEY</h1>
-          <div class="d-flex">
-            <div class="py-4 align-self-end">
-              Whisky casks are growing as a investment oppertunity for the general public and consumers. With casks ranging from several hundreds to tens of thousands of pounds, 
-              this significantly reduces the possibility of the general public having the ability to risk significant savings or simply afford to invest within scotch whisky at all.
-            </div>
-          </div>
-          <a class="browse_button px-5 py-2 border rounded" href="#" role="button">Link</a>
+    <!--HERO-->
+    <div class="hero">
+        <img class="img-fluid" src="./images/glencoe.png" alt="main page background image">
+        <div class="herotext">
+            <h1>Invest in Scotch Whisky</h1>
+            <button class="button hvr-hollow" onclick="window.location.href='casks.php'">Browse our Casks</button>
         </div>
-        
-        <div class="col-lg-4">
-          <img class="rounded img-fluid d-none d-lg-block" src="images/cask1.png" alt="Image of a Scotch Whiskey Cask">
+        <div class="caskimg">
+          <img src="./images/cask1.png" alt="cask">
         </div>
+    </div>
+<!--MOBILE HERO-->
+<div class="mobile-hero">
+  <div class="px-4 py-5 my-5 text-center">
+    <h2 class="display-5 fw-bold">Browse</h2>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Best Pet Supplies in Edinburgh</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" onclick="window.location.href='food_shop.php'">Products</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4 text-dark" onclick="window.location.href='grooming.php'">Grooming</button>
       </div>
     </div>
-  </section>
+  </div>
+</div>
+</div>
 
-  <!-- CAROUSEL SECTION -->
 
-      <!--footer-->
+    <!--footer-->
     <?php
 
     include('./includes/footer.php');
 
     ?>
+
 
 <?php
 
