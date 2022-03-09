76b9314c2c6a91aa773a0e9cd7a817591a564428
Piddl52 <56163393+Piddl52@users.noreply.github.com>
2022-03-09 12:48:43 +0000

    footer updated, cask product page added, header design work across all pages...more changes to come today

---
+++ casks.php
@@ -0,0 +1,74 @@
+<?php
+$PageTitle = "Casks";
+include 'Components/RequiredComponents/header.inc.php';
+include 'Components/RequiredComponents/styles.inc.php';
+?>
+  <?php include 'Components/RequiredComponents/navbar.inc.php'; ?>
+
+      <!-- ICON BOXES -->
+
+  <section id="icon-boxes" class="py-5">
+    <div class="container mb-4">
+      <div class="row">
+        <div class="col-md-4 p-0">
+          <div class="card product-card bg-dark border-light text-white text-center">
+            <div class="card-body">
+            <a href="#"><img class="card-img-top" src="images/cask1.png" alt="Scotch Whiskey Cask Image"></a>
+              <h3>Sample Heading</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card bg-dark border-light text-white text-center">
+            <div class="card-body">
+            <a href="#"><img class="card-img-top" src="images/cask1.png" alt="Scotch Whiskey Cask Image"></a>            
+            <h3>Sample Heading</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card bg-dark border-light text-white text-center">
+            <div class="card-body">
+            <a href="#"><img class="card-img-top" src="images/cask1.png" alt="Scotch Whiskey Cask Image"></a>
+              <h3>Sample Heading</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4 p-0">
+          <div class="card bg-dark border-light text-white text-center">
+            <div class="card-body">
+            <a href="#"><img class="card-img-top" src="images/cask1.png" alt="Scotch Whiskey Cask Image"></a>
+              <h3>Sample Heading</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card bg-dark border-light text-white text-center">
+            <div class="card-body">
+            <a href="#"><img class="card-img-top" src="images/cask1.png" alt="Scotch Whiskey Cask Image"></a>
+              <h3>Sample Heading</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 p-0">
+          <div class="card bg-dark border-light text-white text-center">
+            <div class="card-body">
+            <a href="#"><img class="card-img-top" src="images/cask1.png" alt="Scotch Whiskey Cask Image"></a>
+              <h3>Sample Heading</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php include 'Components/RequiredComponents/footer.inc.php'; ?>
