94b93bf2ae5ec36950ccc55cb013e8784304c6e3
Alan <confusedszn@gmail.com>
2022-03-09 21:17:32 +0000

    Fixed Image Backend Validation

--- Components/DashboardComponents/Admin/CaskManagement/addCask.inc.php
+++ Components/DashboardComponents/Admin/CaskManagement/addCask.inc.php
@@ -16,20 +16,16 @@ $woodType = $_POST['WoodType'];
 $distilleryName = $_POST['DistilleryName'];
 $caskImage = base64_encode(file_get_contents($_FILES["CaskImage"]["tmp_name"]));
 
-$allowed_types =array('jpg','png', 'jpeg')
-$error = null;
+$allowed_types = array('jpg', 'png', 'jpeg');
 
 // Get the file extension
-$extension = pathinfo($caskImage, PATHINFO_EXTENSION);
+$extension = pathinfo($_FILES["CaskImage"]["name"], PATHINFO_EXTENSION);
 
 // Search the array for the allowed file type
-
+//Implement Error Message Here
 if (in_array($extension, $allowed_types, false) != true) {
-
-    $error = "ERROR: ILLEGAL FILE TYPE";
-    return $error; // or use  exit;
+  exit();
 }
-
 
 //Prepared Statement
 $stmt = $dbConnection->prepare("INSERT INTO cask (CaskName, CaskDescription, PercentageAvailable, WholeCaskPrice, OLA, RLA, PercentageAlcohol, CaskType, WoodType, DistilleryName, CaskImage) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
