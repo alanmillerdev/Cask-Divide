9fea5101e90f7b9413f4163ecebe1d5ef9a48d8e
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-03-09 13:06:08 +0000

    Contact page logic added
    Added contact form validation and logic using AJAX, JS and PHP

---
+++ Components/ContactComponents/contactFormLogic.inc.php
@@ -0,0 +1,26 @@
+
+<?php
+
+
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+                header('../../contact.php');
+                $name = strip_tags(htmlspecialchars($_POST['name']));
+                $email_address = strip_tags(htmlspecialchars($_POST['email']));
+                $phoneNumber = strip_tags(htmlspecialchars($_POST['phoneNumber']));
+                $message = strip_tags(htmlspecialchars($_POST['message']));
+   
+                    // Create the email and send the message
+                    $to = 'kelly@apertureits.co.uk'; // Clients email. Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+                    $email_subject = "Cask Divide Website Enquiry:  $name";
+                    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone Number: $phoneNumber\n\nMessage:\n$message";
+                    $headers = "From: kellyhunter-@outlook.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+                    $headers .= "Reply-To: $email_address";   
+                    mail($to,$email_subject,$email_body,$headers);
+                    return true;         
+         }
+
+
+       ?>
--- contact.php
+++ contact.php
@@ -1,50 +1,20 @@
 <?php
 
 $PageTitle = "Contact";
+
+
 include 'Components/RequiredComponents/header.inc.php';
+
 include 'Components/RequiredComponents/styles.inc.php';
 
+include 'Components/ContactComponents/contactDiv.inc.php';
+
+include 'Components/RequiredComponents/navbar.inc.php';
+
+include 'Components/ContactComponents/contactForm.inc.php';
+    
+include 'Components/RequiredComponents/footer.inc.php';
+
+include 'Components/ContactComponents/contactFormLogic.inc.php';  
+
 ?>
-
-<div class="content">
-
-<body data-spy="scroll" data-target="#main-nav" id="home" class="bg-dark">
-
-  <?php include 'Components/RequiredComponents/navbar.inc.php'; ?>
-
-  <!-- CONTACT SECTION -->
-  <section id="form-styling">
-    <div class="container">
-      <div class="d-flex justify-content-center">
-        <div class="rounded-3 text-light bg-dark p-5">
-          <div class="text-center">
-            <h3 class="mt-1 mb-5">Contact</h3>
-          </div>
-
-          <form class="d-flex flex-column">
-            <div class="form-outline mb-4">
-              <input type="email" id="" class="input-style" placeholder="Name" />
-            </div>
-
-            <div class="form-outline mb-4">
-              <input type="" id="" class="input-style" placeholder="Email" />
-            </div>
-
-            <div class="form-outline mb-4">
-              <input type="" id="" class="input-style" placeholder="Phone Number" />
-            </div>
-
-            <div class="form-outline mb-4">
-              <textarea name="" id="" class="input-style" cols="25" rows="2" placeholder="Message"></textarea>
-            </div>
-
-            <div class="text-center pt-1 mb-5 pb-1">
-              <button class="form-styling-button" type="button"><span>Send</span></button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <?php include 'Components/RequiredComponents/footer.inc.php'; ?>
