3522eb807b63cbe906c83d907f23de6734c6f510
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-03-09 17:41:25 +0000

    Updated Cask Divide SQL
    Added a join table to solve the many-to-many relationship between User and Investment.

--- CaskDivide.sql
+++ CaskDivide.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE CaskDivide;
+CREATE DATABASE CaskDivide2;
 
-CREATE TABLE CaskDivide.User(
+CREATE TABLE CaskDivide2.User(
     UserID INTEGER NOT NULL AUTO_INCREMENT,
     Email VARCHAR(255) NOT NULL,
     Password VARCHAR(255) NOT NULL,
@@ -13,43 +13,44 @@ CREATE TABLE CaskDivide.User(
     PRIMARY KEY (UserID)
 );
 
-CREATE TABLE CaskDivide.Admin(
+CREATE TABLE CaskDivide2.Admin(
     UserID INTEGER NOT NULL,
     LastLogin DATETIME NOT NULL,
     PRIMARY KEY (UserID),
     FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
 
-CREATE TABLE CaskDivide.Distillery(
+CREATE TABLE CaskDivide2.Distillery(
     DistilleryName VARCHAR(80) NOT NULL,
     Description VARCHAR(2000) NOT NULL,
     PRIMARY KEY (DistilleryName)
 );
 
-CREATE TABLE CaskDivide.Cask(
+CREATE TABLE CaskDivide2.Cask(
     CaskID INTEGER NOT NULL AUTO_INCREMENT,
     CaskName VARCHAR(80) NOT NULL,
+    CaskDescription VARCHAR(255) NOT NULL,
     PercentageAvailable INTEGER NOT NULL,
-    WholeCaskPrice double(6, 2) NOT NULL,
+    WholeCaskPrice DECIMAL(8, 2) NOT NULL,
     OLA DECIMAL(4, 2) NOT NULL,
     RLA DECIMAL(4, 2) NOT NULL,
     PercentageAlcohol DECIMAL(4, 2) NOT NULL,
     CaskType VARCHAR(60) NOT NULL,
     WoodType VARCHAR(60) NOT NULL,
     DistilleryName VARCHAR(80) NOT NULL,
-    CaskImageLocation VARCHAR(255) NOT NULL,
+    CaskImage LONGTEXT NOT NULL,
     PRIMARY KEY (CaskID),
     FOREIGN KEY (DistilleryName) REFERENCES Distillery(DistilleryName)
 );
 
-CREATE TABLE CaskDivide.Payment(
+CREATE TABLE CaskDivide2.Payment(
     TransactionID INTEGER NOT NULL AUTO_INCREMENT,
     StripeTransactionID VARCHAR(255) NOT NULL,
     StripeCustomerID VARCHAR(255) NOT NULL,
     PRIMARY KEY (TransactionID)
 );
 
-CREATE TABLE CaskDivide.Investments(
+CREATE TABLE CaskDivide2.Investment(
     InvestmentID INTEGER NOT NULL AUTO_INCREMENT,
     UserID INTEGER NOT NULL,
     CaskID INTEGER NOT NULL,
@@ -63,7 +64,17 @@ CREATE TABLE CaskDivide.Investments(
     FOREIGN KEY (TransactionID) REFERENCES Payment(TransactionID)
 );
 
-CREATE TABLE CaskDivide.AdminLogItem(
+CREATE TABLE CaskDivide2.UserInvestments(
+    UserInvestmentID INTEGER NOT NULL AUTO_INCREMENT,
+    UserID INTEGER NOT NULL,
+    InvestmentID INTEGER NOT NULL,
+    PurchaseDate DATETIME NOT NULL,
+    PRIMARY KEY (UserInvestmentID),
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (InvestmentID) REFERENCES Investment(InvestmentID)
+);
+
+CREATE TABLE CaskDivide2.AdminLogItem(
     ID INTEGER NOT NULL AUTO_INCREMENT,
     UserID INTEGER NOT NULL,
     CaskID INTEGER NOT NULL,
