43afb358d987d92c188e3c539bc0c99af7f41d84
mmizeraa <79662823+mmizeraa@users.noreply.github.com>
2022-03-09 15:19:49 +0000

    Image uploader added, to be checked

--- Components/DashboardComponents/Admin/CaskManagement/addCask.inc.php
+++ Components/DashboardComponents/Admin/CaskManagement/addCask.inc.php
@@ -4,6 +4,25 @@ include '../../../../Database/dbConnect.inc.php';
 
 $dbConnection = Connect();
 
+//Define connections for ftp
+if(isset($_POST['add']))
+{
+$tbl_name = 'cask';
+$ftp_user = '';
+$ftp_pass = '';
+$ftp_server = '';
+
+$ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
+//Login with username and password
+$login_result = ftp_login($ftp_conn, $ftp_user, $ftp_pass);
+
+//Check connection
+if ((!$ftp_conn) || (!$login_result)) {
+    echo "FTP connection has failed!";
+    echo "Attempted to connect to $ftp_server for user $ftp_user";
+    exit;
+}
+
 $caskName = $_POST['NameOfCask'];
 $percentageAvailable = $_POST['PercentageAvailable'];
 $wholeCaskPrice = $_POST['WholeCaskPrice'];
@@ -13,6 +32,28 @@ $percentageAlcohol = $_POST['PercentageAlcohol'];
 $caskType = $_POST['CaskType'];
 $woodType = $_POST['WoodType'];
 $distilleryName = $_POST['DistilleryName'];
+$uploadDir = '../';
+$fileName = $_FILES['fileToUpload']['name'];
+$filePath = $uploadDir . $fileName;
+
+//Move uploaded file into the ftp
+if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],"../".$_FILES["fileToUpload"]["name"]))
+{
+//If file has uploaded successfully, store its name in data base
+$query_image = "INSERT INTO $tbl_name(img) VALUES ('$filePath')";
+if(mysqli_query($con,$query_image))
+{
+header("Location: ");
+}
+else
+{
+echo 'File name not stored in database';
+}
+}
+else{echo 'File not uploaded';}
+
+}
+
 
 //Prepared Statement
 $stmt = $dbConnection->prepare("INSERT INTO cask (CaskName, PercentageAvailable, WholeCaskPrice, OLA, RLA, PercentageAlcohol, CaskType, WoodType, DistilleryName) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
