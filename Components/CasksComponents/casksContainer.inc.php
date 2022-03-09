907bc5128c074a22fda68288db55250915dc6725
Alan <confusedszn@gmail.com>
2022-03-09 14:59:54 +0000

    Collaboration Push

---
+++ Components/CasksComponents/casksContainer.inc.php
@@ -0,0 +1,14 @@
+<section id="icon-boxes" class="py-5">
+    <div class="container mb-4">
+        <div class="row">
+
+            <?php
+
+            include('casksViewBuilder.inc.php');
+
+            ?>
+        </div>
+
+    </div>
+    </div>
+</section>
