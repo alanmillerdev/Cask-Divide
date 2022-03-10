6c43417b9364cebb2152d66dd02674816b12baad
Alan <confusedszn@gmail.com>
2022-03-10 10:24:27 +0000

    Attempt to fix live version

--- casks.php
+++ casks.php
@@ -1,12 +1,12 @@
 <?php
 $PageTitle = "Casks";
 include 'Components/RequiredComponents/header.inc.php';
-include 'Components/RequiredComponents/styles.inc.php'; ?>
+include 'Components/RequiredComponents/styles.inc.php';
 
-  <?php include 'Components/RequiredComponents/navbar.inc.php'; ?>
+include 'Components/RequiredComponents/navbar.inc.php';
 
-  <?php include 'Components/CasksComponents/casksHeader.inc.php'; ?>
+include 'Components/CasksComponents/casksHeader.inc.php';
 
-  <?php include 'Components/CasksComponents/casksContainer.inc.php'; ?>
+include 'Components/CasksComponents/casksContainer.inc.php';
 
-  <?php include 'Components/RequiredComponents/footer.inc.php'; ?>
+include 'Components/RequiredComponents/footer.inc.php';
--- index.php
+++ index.php
@@ -8,40 +8,17 @@ include 'Components/RequiredComponents/styles.inc.php';
 
 <div class="content">
 
-
     <?php
 
-    $page = 'home';
     include('Components/RequiredComponents/navbar.inc.php');
-
-    ?>
-
-    <?php
 
     include('Components/IndexComponents/IndexBanner.inc.php');
 
-    ?>
-
-    <?php
-
     include('Components/IndexComponents/IndexAboutUs.inc.php');
-
-    ?>
-    <?php
 
     include('Components/IndexComponents/Carousel/carouselContainer.inc.php');
 
-    ?>
-
-
-    <?php
-
     include('Components/RequiredComponents/footer.inc.php');
-
-    ?>
-
-
-    <?php
 
     include 'Components/RequiredComponents/bootstrapJS.inc.php';
 
