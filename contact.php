b30d572b45747d8f66ac85c68fe2803d6e48324f
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-03-10 18:20:51 +0000

    Removed bootstrapJS.inc.php from Contact Page
    This has been removed as it is included in the footer.inc.php file

--- contact.php
+++ contact.php
@@ -17,6 +17,6 @@ include 'Components/RequiredComponents/footer.inc.php';
 
 include 'Components/ContactComponents/contactFormLogic.inc.php';
 
-include 'Components/RequiredComponents/bootstrapJS.inc.php';
+
 
 ?>
