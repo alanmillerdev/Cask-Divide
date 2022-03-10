e20f2a0a4795dbe6cda5b3c3c8c4f36714a3baf2
Alan <confusedszn@gmail.com>
2022-03-10 11:05:04 +0000

    Fixed big cask issue

--- Components/IndexComponents/Carousel/carouselCask.inc.php
+++ Components/IndexComponents/Carousel/carouselCask.inc.php
@@ -4,7 +4,7 @@ function generateCask($caskName, $caskDescription, $caskImage, $class)
 {
   return <<<HTML
   <div class="$class">
-    <img class="d-block m-auto w-50" src="data:image/jpeg;base64,$caskImage" alt="Carousel Item">
+    <img class="d-block m-auto w-50" src="data:image/jpeg;base64,$caskImage" height="300" width="300" alt="Carousel Item">
     <div class="carousel-caption d-none d-md-block">
         <h5>$caskName</h5>
         <p>$caskDescription</p>
