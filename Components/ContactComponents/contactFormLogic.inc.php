4bd0f9774741f48a2cdbc90eff6d8e18a74cf4c0
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-03-10 19:41:31 +0000

    Fixed Contact Page SecurityCheck errors

--- Components/ContactComponents/contactFormLogic.inc.php
+++ Components/ContactComponents/contactFormLogic.inc.php
@@ -1,8 +1,9 @@
 
 <?php
-if (getenv('REQUEST_METHOD') != "POST") {
-  header("Location: ../../../../index.php");
+if (getenv('REQUEST_METHOD') != "POST" && !defined('SecurityCheck')) {
+  header("Location: ../../index.php");
 }
+
 
 define('SecurityCheck', TRUE);
 
