50523c74746fc482872a9d51ba73eee2737515c1
mmizeraa <79662823+mmizeraa@users.noreply.github.com>
2022-03-23 16:57:25 +0000

    stripe test checkout

---
+++ test-checkout/create-checkout-session.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'vendor/autoload.php';
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+\Stripe\Stripe::setApiKey('sk_test_26PHem9AhJZvU623DfE1x4sd');
+
+header('Content-Type: application/json');
+
+$YOUR_DOMAIN = 'http://localhost:4242/public';
+
+$checkout_session = \Stripe\Checkout\Session::create([
+  'line_items' => [[
+    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+    'price' => '{{PRICE_ID}}',
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+  'success_url' => $YOUR_DOMAIN . '/success.html',
+  'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+]);
+
+header("HTTP/1.1 303 See Other");
+header("Location: " . $checkout_session->url);
