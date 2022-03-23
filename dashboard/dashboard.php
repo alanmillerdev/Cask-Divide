84c9d9ba81137cc4a838d79d1d50544473c61e24
EuanMaxwell2001 <euanmax@gmail.com>
2022-03-23 21:48:14 +0000

    added a content wrapper for things to the right of the sidebar

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -4,7 +4,18 @@ include('../Components/DashboardComponents/Template/header.inc.php');
 
 include('../Components/DashboardComponents/Template/sidebar.inc.php');
 
+?>
+
+<div class="dashboardContent" style="margin-left: 110px;">
+<?php
+
 include('../Components/DashboardComponents/DashboardType/dashboardType.inc.php');
+
+include('../Components/DashboardComponents/Admin/UserManagement/displayUserTable.inc.php');
+?>
+</div>
+
+<?php
 
 include('../Components/DashboardComponents/Template/heading.inc.php'); 
 
