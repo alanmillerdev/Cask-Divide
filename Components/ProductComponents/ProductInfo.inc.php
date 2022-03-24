2abee7367d152b3bea2a6f478a2ad4b391b142fe
Piddl52 <56163393+Piddl52@users.noreply.github.com>
2022-03-24 17:39:42 +0000

    for Alan to review

--- Components/ProductComponents/ProductInfo.inc.php
+++ Components/ProductComponents/ProductInfo.inc.php
@@ -16,6 +16,7 @@ while($row = mysqli_fetch_array($query)) {
     $CaskPrice = $row['WholeCaskPrice'];
 }
 
+
 ?>
 
 <section class="py-5">
@@ -26,18 +27,20 @@ while($row = mysqli_fetch_array($query)) {
                     <h1 class="display-5 fw-bolder text-white mb-2"><?php echo $CaskName ?></h1>
                     <p class="lead fw-normal text-white-50 mb-4"><?php echo $CaskDescription ?></p>
                     
-                    <form method="POST">
+                    <form method="GET" action="CheckoutComponents/checkoutStart.inc.php">
                         <div class="form-group">
                         <label for="formControlRange" class="text-white">How much do you want to Invest?</label>
-                        <input type="range" min="1" max="<?php echo $PercentageAvailable ?>" value="<?php echo $PercentageAvailable ?>" class="form-control-range" id="formControlRange" onInput="$('#rangevalue').html($(this).val())" style="
+                        <input type="range" min="1" max="<?php echo $PercentageRequested ?>" value="<?php echo $PercentageRequested ?>" class="form-control-range" id="formControlRange" onInput="$('#rangevalue').html($(this).val())" style="
                         border-radius: 100px;">
                         </div>
-                    </form>
+                    
 
-                    <h1 class="display-6 fw-bolder text-white mb-4"><span class="text-white" id="rangevalue"><?php echo $PercentageAvailable ?></span>% of the Cask: £<?php echo $CaskPrice ?></h1>
-                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                        <a class="btn btn-primary btn-lg px-4 me-sm-3" href="Components/CheckoutComponents/checkoutStart.inc.php">Purchase <i class="fas fa-shopping-bag"></i></a>
-                        <a class="btn btn-outline-light btn-lg px-4" href="#">Learn about the Casks <i class="fas fa-arrow-alt-circle-down"></i></a>
+                        <h1 class="display-6 fw-bolder text-white mb-4"><span class="text-white" name="percentage" id="rangevalue"><?php echo $PercentageRequested ?></span>% of the Cask: £<?php echo $CaskPrice ?></h1>
+                        <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                        <a class="btn btn-primary btn-lg px-4 me-sm-3" href='Components/CheckoutComponents/CheckoutStart.inc.php?sku="$caskID"' type="submit" >Purchase <i class="fas fa-shopping-bag"></i></a>
+                    </form>    
+                    
+                    <a class="btn btn-outline-light btn-lg px-4" href="#">Learn about the Casks <i class="fas fa-arrow-alt-circle-down"></i></a>
                     </div>
                 </div>
             </div>
