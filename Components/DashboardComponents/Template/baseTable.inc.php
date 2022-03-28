4309163949211ce3c87a70ab22fbd48a3507de8b
EuanMaxwell2001 <euanmax@gmail.com>
2022-03-28 21:12:14 +0100

    added a basic table

---
+++ Components/DashboardComponents/Template/baseTable.inc.php
@@ -0,0 +1,309 @@
+<?php
+if(!defined('SecurityCheck')) {
+    exit(header("Location: ../../../index.php"));
+  }
+?>
+<div class="card shadow border-0 mb-7">
+                    <div class="card-header">
+                        <h5 class="mb-0">New Users</h5>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-nowrap">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Date Created</th>
+                                    <th scope="col">Phone Number</th>
+                                    <th scope="col">Money Invested</th>
+                                    <th scope="col">Date of Birth</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Robert Fox
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Feb 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1610271340738-726e199f0258?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Darlene Robertson
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Apr 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1610878722345-79c5eaf6a48c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Theresa Webb
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Apr 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1612422656768-d5e4ec31fac0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Kristin Watson
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Feb 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1608976328267-e673d3ec06ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Cody Fisher
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Apr 10, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Robert Fox
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Feb 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1610271340738-726e199f0258?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Darlene Robertson
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Apr 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1610878722345-79c5eaf6a48c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Theresa Webb
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Mar 20, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1612422656768-d5e4ec31fac0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Kristin Watson
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Feb 15, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <img alt="..." src="https://images.unsplash.com/photo-1608976328267-e673d3ec06ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                        <a class="text-heading font-semibold" href="#">
+                                            Cody Fisher
+                                        </a>
+                                    </td>
+                                    <td>
+                                        Apr 10, 2021
+                                    </td>
+                                    <td>
+                                        <a class="text-heading font-semibold" href="#">
+                                            07859456524
+                                        </a>
+                                    </td>
+                                    <td>
+                                        $2750
+                                    </td>
+                                    <td>
+                                            09/07/2001
+                                    </td>
+                                    <td class="text-end">
+                                        <a href="#" class="btn btn-sm btn-neutral">View</a>
+                                        <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                            <i class="bi bi-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer border-0 py-5">
+                        <span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
+                    </div>
+                </div>
--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,9 @@ include('Components/DashboardComponents/Template/header.inc.php');
 
 include('Components/DashboardComponents/Template/sidebar.inc.php');
 
-include('Components/DashboardComponents/Template/mainDash.inc.php'); 
+include('Components/DashboardComponents/Template/mainDash.inc.php');
+
+include('Components/DashboardComponents/Template/baseTable.inc.php'); 
 
 include('Components/DashboardComponents/Template/scripts.inc.php'); 
 
