1f7bb053b616eedd9e79a67e6e79605cf0946951
Piddl52 <56163393+Piddl52@users.noreply.github.com>
2022-03-29 16:46:32 +0100

    adding back the Database Password

--- Components/IndexComponents/IndexAboutUs.inc.php
+++ Components/IndexComponents/IndexAboutUs.inc.php
@@ -6,6 +6,9 @@ if(!defined('SecurityCheck')) {
 <section id="explore-section" class="bg-light text-muted py-5">
     <div class="container">
       <div class="row">
+      <div class="col-md-6">
+          <img src="images/logo-1.png" alt="" class="img-fluid mb-3">
+      </div>
         <div class="col-md-6">
           <h3>About Us</h3>
           <div class="d-flex">
@@ -18,11 +21,10 @@ if(!defined('SecurityCheck')) {
           <div class="d-flex">
             <div class="p-4 align-self-end">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
             </div>
           </div>
-        </div>
-        <div class="col-md-6">
-          <img src="images/logo-1.png" alt="" class="img-fluid mb-3">
         </div>
       </div>
     </div>
