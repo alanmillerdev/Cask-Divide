3ceab8c276fab8b3aa2e75905c8146a69752510e
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-04-11 15:49:41 +0100

    Removed else statement on checkoutInput.inc.php

--- Components/CheckoutComponents/checkoutInput.inc.php
+++ Components/CheckoutComponents/checkoutInput.inc.php
@@ -350,15 +350,6 @@ $currentPercentageAvailable = $row[0];
           }
       });
       return true;
-    }
-    else {
-                  // Fail message
-                  $('#success').html("<div class='alert alert-danger'>");
-            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-              .append("</button>");
-            $('#success > .alert-danger').append($("<strong>").text("Sorry. Please try again later."));
-            $('#success > .alert-danger').append('</div>');
-    }
-    
+
   }
 </script>
