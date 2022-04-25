d87a818cee651a6255462ad55c13dc1d03640e8a
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-04-25 12:54:37 +0100

    Added notifications for updating and deleting casks

--- Components/DashboardComponents/Admin/CaskManagement/deleteCask.inc.php
+++ Components/DashboardComponents/Admin/CaskManagement/deleteCask.inc.php
@@ -2,15 +2,29 @@
 if (getenv('REQUEST_METHOD') != "POST") {
   header("Location: ../../../../index.php");
 }
+session_start();
+
 
 define('SecurityCheck', TRUE);
 
 include '../../../../Database/dbConnect.inc.php';
+include("../../../NotificationComponents/notification.inc.php");
 
 $dbConnection = Connect();
 
 $caskID = $_GET["CaskID"];
+$userID = $_SESSION['UserID'];
+
+$SelectQuery = "SELECT CaskName FROM cask WHERE CaskID = '$caskID'";
+$result = mysqli_query($dbConnection, $SelectQuery);
+$row = mysqli_fetch_row($result);
+$caskName = $row[0];
+$name = $_SESSION['FullName'];
+
+createNoti($dbConnection, $userID, "Delete", "$name has deleted a cask: $caskName");
+header('location:../../../../dashboard/show-casks.php');
 
 $DeleteQuery = "DELETE FROM cask WHERE CaskID='$caskID'";
 $result = mysqli_query($dbConnection, $DeleteQuery) or die(mysqli_error($dbConnection));
-header('location:../../../../dashboard/show-casks.php');
+
+
