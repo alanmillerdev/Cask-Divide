644f16a9918bc5d8391cfd01f92367d0fa43168f
spacedaisy <67166576+spacedaisy@users.noreply.github.com>
2022-05-02 21:39:54 +0100

    Edited about us text

--- Components/AboutComponents/aboutSection2.inc.php
+++ Components/AboutComponents/aboutSection2.inc.php
@@ -4,9 +4,8 @@
     <div class="container col-xxl-8 px-4 py-5">
   <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-lg-6">
-      <h1 class="display-5 fw-bold lh-1 mb-3">Our Passion for Whiskey</h1>
-      <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eos accusantium distinctio magni. Perspiciatis soluta saepe, perferendis non eligendi repudiandae qui delectus at aliquam quibusdam aut nemo labore rerum doloremque?
-      Provident, natus non. Similique, perferendis! Voluptatibus, deserunt necessitatibus dolorem et dolore debitis voluptatem nisi voluptate labore ratione eveniet aliquam doloribus, delectus eos quo fugiat esse ad vitae maxime non quos?</p>
+      <h1 class="display-5 fw-bold lh-1 mb-3">Our Story</h1>
+      <p class="lead">Our parent site Bottle Exchange is offering a way to buy, sell and swap valuable bottles. We created Cask Divide to further open up the market and give people the freedom to purchase whiskey cask shares. Our years of experience in the industry have allowed us to develop deep connections so that your investment is in the right hands. </p>
     </div>
     <div class="col-10 col-sm-8 col-lg-6">
       <img src="images/glencoe.png" class="d-block mx-lg-auto img-fluid"  width="700" height="500" loading="lazy" alt="Dog in bath">
