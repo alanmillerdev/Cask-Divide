cf99a983da34fab4712e586dbfc0ccc796a78f94
Alan <confusedszn@gmail.com>
2022-05-02 13:34:07 +0100

    Updated Index Page About Us

--- Components/IndexComponents/IndexAboutUs.inc.php
+++ Components/IndexComponents/IndexAboutUs.inc.php
@@ -1,31 +1,22 @@
 <?php
-if(!defined('SecurityCheck')) {
+if (!defined('SecurityCheck')) {
   exit(header("Location: ../../index.php"));
 }
 ?>
 <section id="explore-section" class="bg-light text-muted py-5">
-    <div class="container">
-      <div class="row">
+  <div class="container">
+    <div class="row">
       <div class="col-md-6">
-          <img src="images/logo-1.png" alt="" class="img-fluid mb-3">
+        <img src="images/logo-1.png" alt="" class="img-fluid mb-3">
       </div>
-        <div class="col-md-6">
-          <h3>About Us</h3>
-          <div class="d-flex">
-            <div class="p-4 align-self-end">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-            </div>
-          </div>
-          <div class="d-flex">
-            <div class="p-4 align-self-end">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio iusto, perspiciatis mollitia natus harum?
-            </div>
+      <div class="col-md-6">
+        <h3>About Us</h3>
+        <div class="d-flex">
+          <div class="p-4 align-self-end">
+            Not everyone has the finances to not only purchase a Whisky Cask outright but cover the required yearly insurance and storage costs. At Cask Divide we operate as a Cask sharing option giving you the opportunity to own a share of a Whisky cask at a significantly reduced cost whilst still having the benefit of owning a cask. This not only removes the extensive financial commitment of owning a cask as a whole but gives you the opportunity to purchase as many shares in as many different casks as you like. Our Single payment service gives you the ability to purchase your cask without the need to constantly monitor the Whisky Market to look for the best time to sell. Our Casks are held then sold as a whole via Online Auction so there is no bottling or shipping charges to consider. Cask holdings will vary in timescales giving you the option to invest as a short or mid-term investment.
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
